aa448e982a9dc7dcccd99ab1c57c7dac878af50b
Thorin Oakenshield <thror1200@gmail.com>
2021-06-07 15:49:12 +0700

    finish create post middleware

--- src/controller/posts.controller.js
+++ src/controller/posts.controller.js
@@ -4,24 +4,29 @@ const Post = require("../model/posts.model");
 
 const postsController = {
   createPost: async (req, res, next) => {
-    const token = req.headers["Authorization"].split(" ")[1];
+    const token = req.headers["authorization"].split(" ")[1];
     if (token) {
       //Check valid token or invalid
-      jwt
-        .verify(token, process.env.ACCESS_TOKEN_SECRET, (error, encoded) => {
-          if (error) {
-            return res.status(403).send(error);
-          }
-          const photos = req.file.map(photo => photo.path);
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (error, encoded) => {
+        if (error) {
+          return res.status(403).send(error);
+        }
+
+        const photos = req.files.map(photo => photo.path);
+        try {
           const post = new Post({
-            postedBy: mongoose.mongo.ObjectId(encoded._id),
+            postedBy: mongoose.mongo.ObjectId("55153a8014829a865bbf700d"),
             content: req.body.content,
             images: photos,
           });
-        })
-        .save()
-        .then(res.json)
-        .catch(next);
+          await post.save();
+          return res.send(post);
+        } catch (error) {
+          next(error);
+        }
+      });
+    } else {
+      res.status(403).send("Unauthorize");
     }
   },
 };
